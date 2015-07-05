7bc669c6d5793c61555f0528141bdbbd030a7102
Zhigang Zhou <zhouzhigang90s@gmail.com>
2015-07-05 09:42:21 -0400

    First Scala program.

---
+++ scala/intro/HelloWorld.scala
@@ -0,0 +1,18 @@
+/**
+ * The first Scala program: print "Hello, World!".
+ *
+ * Compile:
+ *  scala HelloWorld.scala
+ * Compile with target directory
+ *  scalac -d classes HelloWorld.scala
+ *
+ * Run:
+ *  scala HelloWorld
+ * Run with classpath
+ *  scala -cp classes HelloWorld
+ */
+object HelloWorld {
+    def main(args: Array[String]) {
+        println("Hello, World!")
+    }
+}
